ce45e538b298657ec006351b6c97671c0f285052
Pawel Panasewicz <pawel.panasewicz@gmail.com>
2017-04-05 18:57:26 +0200

    data type for counting

--- src/main/scala/cc/CheckoutCounter.scala
+++ src/main/scala/cc/CheckoutCounter.scala
@@ -9,12 +9,12 @@ object CheckoutCounter {
 
   def computeCost(product: Product, products: Product*): Price = {
     val all = product +: products
-    val occurrences: Map[Product, SafeLong] = all.foldLeft(emptyMap)((acc, p) => acc.updated(p, acc(p) + 1))
+    val occurrences: Map[Product, Count] = all.foldLeft(emptyMap)((acc, p) => acc.updated(p, acc(p) + 1))
     cost(occurrences)
   }
 
-  private def cost(occurrences: Map[Product, SafeLong]): Price = occurrences.foldLeft(0: Price)((acc, kv) => acc + (kv._1.price * kv._2))
-  private lazy val emptyMap = Map[Product, SafeLong]().withDefaultValue(0: SafeLong)
+  private def cost(occurrences: Map[Product, Count]): Price = occurrences.foldLeft(0: Price)((acc, kv) => acc + (kv._1.price * kv._2))
+  private lazy val emptyMap = Map[Product, Count]().withDefaultValue(0: Count)
 
 }
 
