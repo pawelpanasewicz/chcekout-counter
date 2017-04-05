b3fe0c85ba8559497e08e2af9ef6ad3137b6b475
Pawel Panasewicz <pawel.panasewicz@gmail.com>
2017-04-05 19:46:39 +0200

    a lil bit of scala doc explaining business requirements behind the implementation

--- src/main/scala/cc/internals/Promo.scala
+++ src/main/scala/cc/internals/Promo.scala
@@ -17,6 +17,10 @@ object Promo {
     if(oranges < apples) atMostCanBeForFree * apple.price else apples * apple.price
   }
 
+  /**
+    * This computes discount for business offer "3 for the price of 2 on Oranges". See test case in order to understand
+    * how it works.
+    */
   def discount2(products: Map[Product, Count]): Price = {
     val oranges = products(orange)
     val howManyOrangesForFree = oranges / 3
